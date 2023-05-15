84525fba95ed3b1e84fb35318c124e81de51159c
joeylam <joey.cm.lam@gmail.com>
2023-05-15 17:57:16 +0800

    update the table structure with default value

--- db-script/account.sql
+++ db-script/account.sql
@@ -1,8 +1,11 @@
+drop table account;
+
+
 create table account (
     account_cd  varchar(20) unique not null,
     account_nm  varchar(100)    not null,
-    updated_by  varchar(20) not null,
-    updated_dt  timestamp   not null
+    updated_by  varchar(20)  not null default current_user,
+    updated_dt  timestamp   not null default current_timestamp
 );
 
 alter table account add constraint account_pk primary key (account_cd);
